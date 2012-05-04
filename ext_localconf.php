d219cdce8cb92bac5fd184cae8579e13a6ac0ac0
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-04 16:44:19 +0000

    [TASK] Add RealURL auto-configuration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/restdoc/trunk@61448 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_restdoc_pi1.php', '_pi1', 'list_type', FALSE);
+
+// RealURL auto-configuration
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/hooks/class.tx_restdoc_realurl.php:tx_restdoc_realurl->addRestdocConfig';
 ?>
---
+++ hooks/class.tx_restdoc_realurl.php
@@ -0,0 +1,66 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Xavier Perseguers <xavier@causal.ch>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * RealURL auto-configuration.
+ *
+ * @category    Hooks
+ * @package     TYPO3
+ * @subpackage  tx_restdoc
+ * @author      Xavier Perseguers <xavier@causal.ch>
+ * @copyright   Causal Sàrl
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ * @version     SVN: $Id$
+ */
+class tx_restdoc_realurl {
+
+	/**
+	 * Generates additional RealURL configuration and merges it with provided configuration.
+	 *
+	 * @param array $params
+	 * @param tx_realurl_autoconfgen $pObj
+	 * @return array
+	 */
+	public function addRestdocConfig(array $params, tx_realurl_autoconfgen $pObj) {
+		return array_merge_recursive($params['config'], array(
+			'postVarSets' => array(
+				'_DEFAULT' => array(
+					'doc' => array(
+						array(
+							'GETvar' => 'tx_restdoc_pi1[doc]',
+						),
+					),
+				),
+			),
+		));
+	}
+
+}
+
+
+if (defined('TYPO3_MODE') && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/restdoc/hooks/class.tx_restdoc_realurl.php'])) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/restdoc/hooks/class.tx_restdoc_realurl.php']);
+}
+
+?>
