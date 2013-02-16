58ac5378302788421e9c32bfca8cf295063a1190
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-16 13:27:25 +0000

    [TASK] Update md5 checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/restdoc/trunk@71265 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "restdoc".
-#
-# Auto generated 05-05-2012 20:34
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "restdoc".
+ *
+ * Auto generated 16-02-2013 14:27
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'reST Documentation Viewer',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"6bbe";s:12:"ext_icon.gif";s:4:"d53d";s:17:"ext_localconf.php";s:4:"38c6";s:14:"ext_tables.php";s:4:"0029";s:16:"locallang_db.xml";s:4:"56b0";s:14:"doc/manual.sxw";s:4:"ae61";s:34:"hooks/class.tx_restdoc_realurl.php";s:4:"8e08";s:28:"pi1/class.tx_restdoc_pi1.php";s:4:"0f42";s:16:"pi1/flexform.xml";s:4:"1a46";s:17:"pi1/locallang.xml";s:4:"bb2c";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"18cf";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"d9cd";s:12:"ext_icon.gif";s:4:"d53d";s:17:"ext_localconf.php";s:4:"38c6";s:14:"ext_tables.php";s:4:"0029";s:16:"locallang_db.xml";s:4:"a3d5";s:14:"doc/manual.sxw";s:4:"4c81";s:34:"hooks/class.tx_restdoc_realurl.php";s:4:"8e08";s:28:"pi1/class.tx_restdoc_pi1.php";s:4:"417f";s:16:"pi1/flexform.xml";s:4:"1fcc";s:17:"pi1/locallang.xml";s:4:"2dab";s:20:"static/constants.txt";s:4:"656f";s:16:"static/setup.txt";s:4:"56c8";}',
 	'suggests' => array(
 	),
 );
