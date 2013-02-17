8daa6716495db1585c32738602606eabcc124f51
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-17 17:31:47 +0000

    [TASK] Add TypoScript examples
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/restdoc/trunk@71280 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ examples/center-images.ts
@@ -0,0 +1,12 @@
+# Center images that are wider than 100px
+plugin.tx_restdoc_pi1.setup.BODY.image.renderObj {
+	stdWrap {
+		preCObject = TEXT
+		preCObject.value = <div style="text-align:center;">
+		preCObject.if.value.data = TSFE:lastImageInfo|0
+		preCObject.if.isGreaterThan = 100
+		
+		postCObject < .preCObject
+		postCObject.value = </div>
+	}
+}
