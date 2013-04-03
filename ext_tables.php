a294bf769322ba79c1b9fd9afd397e6d122adc2c
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-03 22:53:34 +0000

    [TASK] Add an icon to the new content element wizard
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/restdoc/trunk@74251 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -20,4 +20,8 @@ t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/p
 // Initialize static extension templates
 t3lib_extMgm::addStaticFile($GLOBALS['_EXTKEY'], 'static/', 'reST Documentation Viewer');
 
+if (TYPO3_MODE === 'BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_' . $_EXTKEY . '_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY) . 'pi1/class.tx_restdoc_pi1_wizicon.php';
+}
+
 ?>
---
+++ pi1/class.tx_restdoc_pi1_wizicon.php
@@ -0,0 +1,68 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Xavier Perseguers <xavier@causal.ch>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Class that adds the wizard icon.
+ *
+ * @category    Plugin
+ * @package     TYPO3
+ * @subpackage  tx_restdoc
+ * @author      Xavier Perseguers <xavier@causal.ch>
+ * @copyright   Causal Sàrl
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ * @version     SVN: $Id$
+ */
+class tx_restdoc_pi1_wizicon {
+
+	/**
+	 * Processes the wizard items array.
+	 *
+	 * @param array $wizardItems The wizard items
+	 * @return array Modified array with wizard items
+	 */
+	public function proc(array $wizardItems) {
+		$LL = $this->includeLocalLang();
+
+		$wizardItems['plugins_tx_restdoc_pi1'] = array(
+			'icon'        => t3lib_extMgm::extRelPath('restdoc') . 'res/pi1_ce_wizard.png',
+			'title'       => $GLOBALS['LANG']->getLLL('pi1_title', $LL),
+			'description' => $GLOBALS['LANG']->getLLL('pi1_plus_wiz_description', $LL),
+			'params'      => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=restdoc_pi1'
+		);
+
+		return $wizardItems;
+	}
+
+	/**
+	 * Reads the extension locallang.xml and returns the $LOCAL_LANG array found in that file.
+	 *
+	 * @return array The array with language labels
+	 */
+	protected function includeLocalLang() {
+		$llFile = t3lib_extMgm::extPath('restdoc') . 'pi1/locallang.xml';
+		return $GLOBALS['LANG']->includeLLFile($llFile, FALSE);
+	}
+}
+
+?>
