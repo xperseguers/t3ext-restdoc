b7139ea6fe3ef0fb406203ddcd7551984d30b1db
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-04 12:40:23 +0000

    [TASK] Update md5 checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/restdoc/trunk@74268 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "restdoc".
  *
- * Auto generated 07-03-2013 16:31
+ * Auto generated 04-04-2013 14:38
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.2.0-dev',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.1.99',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"d554";s:16:"ext_autoload.php";s:4:"752c";s:12:"ext_icon.gif";s:4:"d53d";s:17:"ext_localconf.php";s:4:"f7cf";s:14:"ext_tables.php";s:4:"0029";s:14:"ext_tables.sql";s:4:"11cb";s:16:"locallang_db.xml";s:4:"d980";s:14:"doc/manual.sxw";s:4:"9ea7";s:25:"examples/center-images.ts";s:4:"7392";s:42:"hooks/class.tx_restdoc_ddgooglesitemap.php";s:4:"08cf";s:34:"hooks/class.tx_restdoc_realurl.php";s:4:"8e08";s:30:"hooks/class.tx_restdoc_toc.php";s:4:"78cb";s:32:"lib/class.tx_restdoc_utility.php";s:4:"12e4";s:28:"pi1/class.tx_restdoc_pi1.php";s:4:"3071";s:16:"pi1/flexform.xml";s:4:"3234";s:17:"pi1/locallang.xml";s:4:"2fea";s:20:"static/constants.txt";s:4:"7837";s:16:"static/setup.txt";s:4:"e74a";}',
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"a67a";s:16:"ext_autoload.php";s:4:"873e";s:12:"ext_icon.gif";s:4:"d42b";s:17:"ext_localconf.php";s:4:"80c3";s:14:"ext_tables.php";s:4:"06ff";s:14:"ext_tables.sql";s:4:"11cb";s:51:"Classes/ContentObject/RestMetadataContentObject.php";s:4:"683f";s:47:"Classes/Controller/Pi1/class.tx_restdoc_pi1.php";s:4:"1e6c";s:55:"Classes/Controller/Pi1/class.tx_restdoc_pi1_wizicon.php";s:4:"b618";s:32:"Classes/Hook/TableOfContents.php";s:4:"3041";s:39:"Classes/Hook/TxDdgooglesitemapPages.php";s:4:"fe34";s:34:"Classes/Hook/TxRealurlAutoconf.php";s:4:"951a";s:44:"Classes/Utility/class.tx_restdoc_utility.php";s:4:"f910";s:26:"Classes/Utility/Helper.php";s:4:"8fa3";s:40:"Configuration/FlexForms/flexform_pi1.xml";s:4:"bbc9";s:38:"Configuration/TypoScript/constants.txt";s:4:"fb2c";s:34:"Configuration/TypoScript/setup.txt";s:4:"3f8c";s:43:"Resources/Private/Examples/center-images.ts";s:4:"7392";s:36:"Resources/Private/Examples/footer.ts";s:4:"bb9a";s:40:"Resources/Private/Language/locallang.xlf";s:4:"b082";s:40:"Resources/Private/Language/locallang.xml";s:4:"3eb3";s:43:"Resources/Private/Language/locallang_db.xlf";s:4:"4231";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"fd57";s:40:"Resources/Public/Icons/pi1_ce_wizard.png";s:4:"7240";s:39:"Resources/Public/JavaScript/doctools.js";s:4:"5ff5";s:42:"Resources/Public/JavaScript/searchtools.js";s:4:"494b";s:41:"Resources/Public/JavaScript/underscore.js";s:4:"b538";s:14:"doc/manual.sxw";s:4:"e84e";s:20:"static/constants.txt";s:4:"665b";s:16:"static/setup.txt";s:4:"463c";}',
 	'suggests' => array(
 	),
 );
