9ffe3481c924d7833d2626a3f8eae2797f433848
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-15 06:58:26 +0000

    [BUGFIX] Class Tx_Restdoc_Reader_SphinxJson is not found with TYPO3 4.5 LTS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/restdoc/trunk@75354 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -4,6 +4,7 @@ $extensionClassesPath = $extensionPath . 'Classes/';
 return array(
 	'tx_restdoc_utility' => $extensionClassesPath . 'Utility/class.tx_restdoc_utility.php',
 	// Compatibility with TYPO3 4.5
+	'tx_restdoc_reader_sphinxjson' => $extensionClassesPath . 'Reader/SphinxJson.php',
 	'tx_restdoc_utility_helper' => $extensionClassesPath . 'Utility/Helper.php',
 );
 ?>
