d81d9b6ba2a19cddbe977ed20e86ed4a4e725185
Xavier Perseguers <xavier@typo3.org>
2015-05-25 09:24:19 +0200

    [TASK] Ensure "restdoc" is always loaded after "realurl"
    Change-Id: I4ebc4860389bb4f1cefb58417b9773034cb12822
    Related: #51069
    Related: #67121
    Reviewed-on: http://review.typo3.org/39695
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- Documentation/KnownProblems/Index.rst
+++ Documentation/KnownProblems/Index.rst
@@ -11,10 +11,6 @@
 Known problems
 ==============
 
-- Hook into EXT:realurl is only possible when this extension is registered *after* EXT:realurl (please
-  read :forge:`67121` for details). If needed, you will need to manually edit :file:`typo3conf/PackageStates.php` and
-  move definition of "restdoc" after the one for "realurl".
-
 - Only FAL LocalStorage has been implemented and tested, meaning code will need to be adapted in order to deal with other
   types of remote storage.
 
--- ext_localconf.php
+++ ext_localconf.php
@@ -24,23 +24,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration'][$_EXTKEY] =
 		'Causal\\Restdoc\\Hooks\\Realurl->registerDefaultConfiguration';
 
-	// RealURL is seriously broken when there is no existing configuration and it
-	// should be auto-configured since registering the hook below will have the
-	// effect of preventing the auto-configuration to take place and every page will
-	// be encoded with the uid as segment only!
-	if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'])) {
-		$message = 'Extension ' . $_EXTKEY . ' is loaded after "realurl". ';
-		$message .= 'Hooks into "realurl" have been disabled to prevent breaking it completely ';
-		$message .= '(please see https://forge.typo3.org/issues/67121 for details). ';
-		$message .= 'To fix this issue, you will have to manually edit file typo3conf/PackageStates.php ';
-		$message .= 'and move the "realurl" block BEFORE the "' . $_EXTKEY . '" one.';
-		\TYPO3\CMS\Core\Utility\GeneralUtility::sysLog(
-			$message,
-			$_EXTKEY,
-			\TYPO3\CMS\Core\Utility\GeneralUtility::SYSLOG_SEVERITY_ERROR
-		);
-	} else {
-		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['decodeSpURL_preProc'][] =
-			'Causal\\Restdoc\\Hooks\\Realurl->decodeSpURL_preProc';
-	}
+	// Hook to grant access to reStructuredText source files
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['decodeSpURL_preProc'][] =
+		'Causal\\Restdoc\\Hooks\\Realurl->decodeSpURL_preProc';
 }
