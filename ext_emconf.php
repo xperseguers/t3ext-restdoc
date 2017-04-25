67c0803820c8ebe72c39e30a52226271ae073e12
Xavier Perseguers <xavier@typo3.org>
2017-04-25 08:47:57 +0200

    [TASK] Adapt dependencies
    Change-Id: I5f372ea1516528338d2ec2ef3a2178f5ab0d53a4
    Reviewed-on: https://review.typo3.org/52589
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.7.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.3-7.0.99',
-            'typo3' => '6.2.0-8.99.99',
+            'php' => '5.3.3-7.1.99',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
