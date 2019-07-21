59bb40be88b930472944a752c9950b88933bb9b1
Xavier Perseguers <xavier@causal.ch>
2019-07-21 12:19:25 +0200

    [TASK] Adapt PHP constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.7.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.3-7.1.99',
+            'php' => '5.3.3-7.3.99',
             'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(),
