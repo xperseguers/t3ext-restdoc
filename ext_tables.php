efdc67a367f8fd75c0327bdf5ab3365c9b4b3a56
Xavier Perseguers <xavier@causal.ch>
2019-11-11 17:36:49 +0100

    [BUGFIX] Remove deprecated call to ExtensionManagementUtility::extRelPath()

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,7 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
     'LLL:EXT:restdoc/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi1',
-    $_EXTKEY . '_pi1',
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif'
+    $_EXTKEY . '_pi1'
 ], 'list_type');
 
 // Register the FlexForms
