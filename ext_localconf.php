5f733400cb1fb3031e0fc470ca247f0e2f26d3fe
Xavier Perseguers <xavier@causal.ch>
2020-03-05 07:59:30 +0100

    [TASK] Migrate registration of plugin for new content element

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,20 @@ $boot = function ($_EXTKEY) {
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Controller/Pi1/Pi1Controller.php', '_pi1', 'list_type', (bool)$config['cache_plugin_output']);
 
+    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'extensions-restdoc-wizard',
+        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        [
+            'source' => 'EXT:restdoc/Resources/Public/Icons/pi1_ce_wizard.png',
+        ]
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:restdoc/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig">'
+    );
+
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('dd_googlesitemap')) {
         // Hook for integrating ReStructured documentation into the Google Sitemap (requires EXT:dd_googlesitemap)
         $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['dd_googlesitemap']['generateSitemapForPagesClass'][] = 'Causal\\Restdoc\\Hooks\\TxDdgooglesitemap';
