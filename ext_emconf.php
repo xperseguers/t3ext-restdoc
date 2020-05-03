05c7e7175486835f9e7b3b317f0e1c1f727a553a
Xavier Perseguers <xavier@causal.ch>
2020-05-03 05:53:43 +0200

    [TASK] Mark extension as compatible with TYPO3 v10 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-7.4.99',
-            'typo3' => '8.7.0-10.3.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
